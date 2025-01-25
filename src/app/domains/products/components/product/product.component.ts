08af32531c54963f12051752b053c18f21aad1b8
Ruben Marinez <me@rubenmarinez.com>
2025-01-25 12:03:04 -0600

    creando el componente product

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-
+  img = 'https://picsum.photos/640/640?r=' + Math.random();
 }
